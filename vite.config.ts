2eccd1a091d5ac47d2f4276facdf4b37629926c1
Davit Vekua <davitvekua23@gmail.com>
2026-04-24 09:27:28 +0200

    build all html files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/cv-portfolio/",
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        anschreiben: resolve(__dirname, "anschreiben.html"),
+        impressum: resolve(__dirname, "impressum.html"),
+        lebenslauf: resolve(__dirname, "lebenslauf.html"),
+      },
+    },
+  },
 });
